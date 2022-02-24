b438de84d5c4f0854542b3872a1cd3617c709474
Dima Korotin <dskorotin@gmail.com>
2022-02-24 15:50:24 +0300

    changes

--- src/main/kotlin/telegram/rent/bot/rent/parsing/Worker.kt
+++ src/main/kotlin/telegram/rent/bot/rent/parsing/Worker.kt
@@ -1,24 +1,17 @@
 package telegram.rent.bot.rent.parsing
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
 import telegram.rent.bot.rent.infrastructure.Apartment
 import telegram.rent.bot.rent.parsing.parser.KufarParser
 import telegram.rent.bot.rent.parsing.parser.OnlinerParser
 import telegram.rent.bot.rent.parsing.parser.RealtParser
 
-object Worker : CoroutineScope by CoroutineScope(Dispatchers.IO) {
+object Worker {
     private const val RESTART_DELAY = 10000L
-    private val apartments = MutableSharedFlow<Apartment>(200)
     private val logger = LoggerFactory.getLogger(this::class.java)
     private val parsers = listOf<Parser>(
         KufarParser(),
@@ -26,29 +19,21 @@ object Worker : CoroutineScope by CoroutineScope(Dispatchers.IO) {
         OnlinerParser()
     )
 
-    suspend fun start(body: suspend (Apartment) -> Unit) = coroutineScope {
-        launch {
-            apartments.collect {
-                body(it)
-            }
-        }
-        launch {
-            while (isActive) {
-                emit()
+    suspend fun start(body: suspend (Apartment) -> Unit) =
+        try {
+            while (true) {
+                emit(body)
                 delay(RESTART_DELAY)
             }
-        }
-    }
-
-    private suspend fun emit() = coroutineScope {
-        try {
-            parsers
-                .map { parser -> async { parser.parse() } }.awaitAll()
-                .flatten()
-                .sortedBy { it.announcement.updatedAt }
-                .forEach { apartments.emit(it) }
         } catch (logging: Exception) {
             logger.debug("Apartment parsing exception:" + logging.message)
         }
+
+    private suspend fun emit(body: suspend (Apartment) -> Unit) = coroutineScope {
+        parsers
+            .map { parser -> async { parser.parse() } }.awaitAll()
+            .flatten()
+            .sortedBy { it.announcement.updatedAt }
+            .forEach { body(it) }
     }
 }
